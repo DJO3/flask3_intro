b023d2327cef239de0b29f704df28ffd51e6044f
Dave O'Connor <oconnorwork@gmail.com>
2015-02-01 14:20:50 -0500

    Index and shopping list created

---
+++ shopping.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+shopping_list = ['Milk', 'Eggs']
+
+
+# Root URL
+@app.route('/')
+def index():
+    return render_template('index.html', items=shopping_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
